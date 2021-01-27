8da7172aae2846a88863949e51a74ad867878628
lololau <verhille.lauriane@gmail.com>
2021-01-27 18:41:18 +0100

    add recipe modifications

--- src/containers/recipe/recipe.tsx
+++ src/containers/recipe/recipe.tsx
@@ -1,46 +1,66 @@
 import { useTranslation } from 'react-i18next/';
 import '../../i18n';
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import PersonIcon from '@material-ui/icons/Person';
 
 const MyRecipe = (): JSX.Element => {
     const { t } = useTranslation();
 
     return (
-        <div>
-            <h1>{t('recipe.title')}</h1>
-            <div className="image_and_time_table">
-                <img
+        <Container>
+            <Grid container style={{ alignItems: 'center' }}>
+                <Grid>
+                    <h1>{t('recipe.title')}</h1>
+                </Grid>
+                <Grid>
+                    <EditIcon style={{ fontSize: 20 }} />
+                </Grid>
+            </Grid>
+            <Grid className="image_and_time_table" style={{ width: '100%' }}>
+                <Avatar
+                    variant="rounded"
+                    style={{ width: 250, height: 200, alignItems: 'center' }}
                     alt="pate_carbonara"
                     src="https://img.static-rmg.be/a/food/image/q75/w1280/h720/1086366/spaghetti-carbonara.jpg"
                 />
-                <div className="time_and_parts_table">
-                    <div className="preparation_time">
+                <Grid container spacing={4} className="time_and_parts_table">
+                    <Grid item className="preparation_time">
                         <h3>Preparation</h3>
                         <p>20 min</p>
-                    </div>
-                    <div className="cooking_time">
+                    </Grid>
+                    <Grid item className="cooking_time">
                         <h3>Cooking</h3>
                         <p>15 min</p>
-                    </div>
-                    <div className="parts">
-                        <h3>Parts</h3>
-                        <p>2</p>
-                    </div>
-                </div>
-            </div>
-            <div>
+                    </Grid>
+                    <Grid item className="parts">
+                        <Grid>
+                            <p>2</p>
+                        </Grid>
+                        <Grid>
+                            <PersonIcon />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid>
                 <h3>Ingredients Component List</h3>
                 <p>Ingredient 1</p>
                 <p>blublu</p>
-            </div>
-            <div className="cooking_instructions">
+            </Grid>
+            <Grid className="cooking_instructions">
                 <h3>Cooking Instructions</h3>
                 <img
                     alt="pates_carbonara_instructions"
                     src="../../../public/Capture d’écran 2021-01-14 à 16.37.29.png"
                 />
-            </div>
-        </div>
+            </Grid>
+        </Container>
     );
 };
 
