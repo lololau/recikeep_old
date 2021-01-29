588a0b4f2a4f93ebb372622eec36a7b9c99e679d
lololau <verhille.lauriane@gmail.com>
2021-01-29 11:55:14 +0100

    Add modal ingredient into my-ingredients file

--- src/containers/my-ingredients/my_ingredients.tsx
+++ src/containers/my-ingredients/my_ingredients.tsx
@@ -4,12 +4,12 @@ import { useTranslation } from 'react-i18next';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Button, IconButton } from '@material-ui/core';
+import { Button, IconButton, TextField } from '@material-ui/core';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import Grid from '@material-ui/core/Grid';
-import Modal from '@material-ui/core/Modal';
+import { Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
 
 type IngredientsProps = {
     ingredients: ingredient[];
@@ -45,20 +45,46 @@ const MyIngredients = (): JSX.Element => {
     const [modalOpen, setModalOpen] = useState(false);
     return (
         <Container>
-            <h1>{t('myIngredients.title-page')}</h1>
-            <IngredientsList ingredients={myIngredients} />
-            <IconButton style={{ width: '100%' }} onClick={() => setModalOpen(true)}>
-                <Grid container direction="column" alignItems="center" spacing={1}>
-                    <AddCircleOutlineOutlinedIcon style={{ fontSize: 30 }} />
-                    <p style={{ fontSize: 11 }}>{t('myIngredients.add-ingredient')}</p>
+            <Grid container style={{ alignItems: 'center' }}>
+                <Grid item xs={6}>
+                    <h1>{t('myIngredients.title-page')}</h1>
                 </Grid>
-            </IconButton>
-            <Modal open={modalOpen}>
+                <Grid item xs={6} style={{ textAlign: 'right', alignItems: 'center' }}>
+                    <IconButton onClick={() => setModalOpen(true)}>
+                        <AddCircleOutlineOutlinedIcon style={{ fontSize: 30 }} />
+                    </IconButton>
+                </Grid>
+            </Grid>
+            <IngredientsList ingredients={myIngredients} />
+
+            <Dialog open={modalOpen} style={{}}>
                 <Container>
-                    <h3>blublu</h3>
-                    <Button onClick={() => setModalOpen(false)}>Close</Button>
+                    <DialogTitle>
+                        <h3>{t('myIngredients.add-ingredient')}</h3>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogActions>
+                            <Grid container spacing={4} style={{ flexDirection: 'column' }}>
+                                <Grid item xs>
+                                    <Grid container spacing={6} style={{ alignItems: 'center' }}>
+                                        <Grid item xs={6}>
+                                            <TextField placeholder={t('myIngredients.textField')} />
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Button>{t('myIngredients.modal-add')}</Button>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs>
+                                    <Button onClick={() => setModalOpen(false)}>
+                                        {t('myIngredients.modal-close')}
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </DialogActions>
+                    </DialogContent>
                 </Container>
-            </Modal>
+            </Dialog>
         </Container>
     );
 };
