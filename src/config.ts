d284e39e645f8d0a52d52347e663683ccefb58c5
lololau <verhille.lauriane@gmail.com>
2021-02-16 17:18:35 +0100

    update config tracking

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+// Firebase Config
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+};
+
+firebase.initializeApp(config);
