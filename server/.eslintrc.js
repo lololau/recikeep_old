174c9756e9ea445db718643bb466008d94ec2553
lololau <verhille.lauriane@gmail.com>
2021-02-17 15:56:45 +0100

    add server ci and remove old README file from client folder

---
+++ server/.eslintrc.js
@@ -0,0 +1,25 @@
+// From : https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project?fbclid=IwAR0blOeTkGy742OkmpHxPeOKaa7V1PHOl8vVFD06xKIs_v1P8bgYWGbkM2k
+
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    settings: {
+        react: {
+            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+        },
+    },
+    extends: [
+        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    rules: {
+        'react/prop-types': 0,
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+};
