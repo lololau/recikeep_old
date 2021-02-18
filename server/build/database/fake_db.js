052c408637812644217269473a86e0013eb4bbbe
lololau <verhille.lauriane@gmail.com>
2021-02-18 15:15:13 +0100

    database

--- server/build/database/fake_db.js
+++ server/build/database/fake_db.js
@@ -6,9 +6,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var sqlite3_1 = __importDefault(require("sqlite3"));
 var db = new sqlite3_1.default.Database(process.env.TEST_DATABASE || './database.sqlite');
 db.serialize(function () {
-    db.run("INSERT INTO User (username, date_creation, date_update) VALUES ('Lololapin', '2020-08-22', '2021-02-17')");
-    db.run("INSERT INTO Recipe (name, number_parts, date_creation, date_update) VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17')"); /*
-    db.run(
-        `INSERT INTO Groups (name, number_parts, date_creation, date_update) VALUES ('Pates saumon', 2, new Date(2020, 8, 22), new Date(2021, 2, 17))`,
-    ); */
+    db.run("INSERT INTO User (username, date_creation, date_update) VALUES ('Lololapin', '2020-08-22', '2021-02-17')")
+        .get("SELECT id FROM User", function (err, row) {
+        if (err) {
+            throw err;
+        }
+        var userId = row.id;
+        console.log(userId);
+        db.run("INSERT INTO Recipe (name, number_parts, date_creation, date_update, user_id) VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17', " + userId + ")");
+    })
+        .run("INSERT INTO Groups (name, date_creation, date_update) VALUES ('Beeboo Recipes', '2021-02-17', '2021-02-17')");
+    db.serialize(function () {
+        var groupId;
+        var recipeId;
+        db.get("SELECT id FROM Groups", function (err, row) {
+            if (err) {
+                throw err;
+            }
+            groupId = row.id;
+            console.log('groupId: ', groupId);
+        }).get("SELECT id FROM Recipe", function (err, row) {
+            if (err) {
+                throw err;
+            }
+            recipeId = row.id;
+            console.log(recipeId);
+            db.run("INSERT INTO Recipe_group (group_id, recipe_id) VALUES ('" + groupId + "', '" + recipeId + "')");
+        });
+    });
 });
+/* db.run(
+    `INSERT INTO Recipe (name, number_parts, date_creation, date_update, user_id) VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17', ${userId})`,
+); */
--- server/src/database/fake_db.ts
+++ server/src/database/fake_db.ts
@@ -4,9 +4,35 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 db.serialize(() => {
     db.run(`INSERT INTO User (username, date_creation, date_update) VALUES ('Lololapin', '2020-08-22', '2021-02-17')`);
     db.run(
-        `INSERT INTO Recipe (name, number_parts, date_creation, date_update) VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17')`,
-    ); /* 
+        `INSERT INTO Recipe (name, number_parts, date_creation, date_update, user_id) 
+        VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17')       
+        SELECT id as user_id FROM User`,
+    );
     db.run(
-        `INSERT INTO Groups (name, number_parts, date_creation, date_update) VALUES ('Pates saumon', 2, new Date(2020, 8, 22), new Date(2021, 2, 17))`,
-    ); */
+        `INSERT INTO Groups (name, date_creation, date_update) VALUES ('Beeboo Recipes', '2021-02-17', '2021-02-17')`,
+    );
+
+    db.serialize(() => {
+        let groupId: number;
+        let recipeId: number;
+
+        db.get(`SELECT id FROM Groups`, (err, row) => {
+            if (err) {
+                throw err;
+            }
+            groupId = row.id;
+            console.log('groupId: ', groupId);
+        }).get(`SELECT id FROM Recipe`, (err, row) => {
+            if (err) {
+                throw err;
+            }
+            recipeId = row.id;
+            console.log(recipeId);
+
+            db.run(`INSERT INTO Recipe_group (group_id, recipe_id) VALUES ('${groupId}', '${recipeId}')`);
+        });
+    });
 });
+/* db.run(
+    `INSERT INTO Recipe (name, number_parts, date_creation, date_update, user_id) VALUES ('Pates saumon', 2, '2021-02-17', '2021-02-17', ${userId})`,
+); */
