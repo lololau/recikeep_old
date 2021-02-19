be82bc5bc7b9b1903470745c6598d176c5cc99c4
lololau <verhille.lauriane@gmail.com>
2021-02-19 16:28:32 +0100

    Add populate database

--- server/build/database/migration.js
+++ server/build/database/migration.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var sqlite3_1 = __importDefault(require("sqlite3"));
 var db = new sqlite3_1.default.Database(process.env.TEST_DATABASE || './database.sqlite');
 db.serialize(function () {
-    db.run('DROP TABLE IF EXISTS Recipe');
-    db.run("CREATE TABLE Recipe (\n        id INTEGER UNIQUE,\n        name TEXT UNIQUE NOT NULL,\n        presentation TEXT,\n        number_parts INTEGER NOT NULL,\n        time_presentation TEXT,\n        time_cooking TEXT,\n        date_creation DATE NOT NULL,\n        date_update DATE NOT NULL,\n        user_id INTEGER,\n        recipe_photo_id INTEGER,\n        recipe_description_id INTEGER,\n        PRIMARY KEY(\"id\" AUTOINCREMENT),\n        FOREIGN KEY(user_id) REFERENCES User(id),\n        FOREIGN KEY(recipe_photo_id) REFERENCES Recipe_photo(id),\n        FOREIGN KEY(recipe_description_id) REFERENCES Recipe_description(id)\n    )");
     db.run('DROP TABLE IF EXISTS User');
     db.run("CREATE TABLE User (\n        id INTEGER UNIQUE,\n        username TEXT UNIQUE,\n        firebaseId TEXT UNIQUE,\n        image BLOB,\n        date_creation DATE,\n        date_update DATE,\n        PRIMARY KEY(\"id\" AUTOINCREMENT)\n    )");
+    db.run('DROP TABLE IF EXISTS Recipe');
+    db.run("CREATE TABLE Recipe (\n        id INTEGER UNIQUE,\n        name TEXT UNIQUE NOT NULL,\n        presentation TEXT,\n        number_parts INTEGER NOT NULL,\n        time_presentation TEXT,\n        time_cooking TEXT,\n        date_creation DATE NOT NULL,\n        date_update DATE NOT NULL,\n        user_id INTEGER,\n        recipe_photo_id INTEGER,\n        recipe_description_id INTEGER,\n        PRIMARY KEY(\"id\" AUTOINCREMENT),\n        FOREIGN KEY(user_id) REFERENCES User(id),\n        FOREIGN KEY(recipe_photo_id) REFERENCES Recipe_photo(id),\n        FOREIGN KEY(recipe_description_id) REFERENCES Recipe_description(id)\n    )");
     db.run('DROP TABLE IF EXISTS Recipe_photo');
     db.run("CREATE TABLE Recipe_photo (\n        id INTEGER UNIQUE,\n        image BLOB,\n        PRIMARY KEY(\"id\" AUTOINCREMENT)\n    )");
     db.run('DROP TABLE IF EXISTS Recipe_description');
