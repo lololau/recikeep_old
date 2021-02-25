7ed2fd26cc1e34bdb84fdc258bf03f25cc9c8016
lololau <verhille.lauriane@gmail.com>
2021-02-25 18:18:21 +0100

    set firebase server side

--- client/src/slice/userSlice.ts
+++ client/src/slice/userSlice.ts
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../app/store';
+import firebase from 'firebase/app';
 
 export interface User {
     id: number;
     firebaseId: string;
+    idToken: string;
     username: string;
     email: string;
     image_profile?: Blob;
@@ -12,6 +14,7 @@ export interface User {
 const initialState: User = {
     id: 0,
     firebaseId: '',
+    idToken: '',
     username: 'laulau',
     email: 'verhille.lauriane@gmail.com',
 };
@@ -23,12 +26,16 @@ const userReducer = createSlice({
         updateFirebaseId: (state, action) => {
             state.firebaseId = action.payload;
         },
+        updateIdToken: (state, action) => {
+            state.idToken = action.payload;
+        },
     },
 });
 
-export const { updateFirebaseId } = userReducer.actions;
+export const { updateFirebaseId, updateIdToken } = userReducer.actions;
 
 export const selectUser = (state: RootState): User => state.user;
 export const isLogged = (state: RootState): boolean => state.user.firebaseId !== '';
+export const token = (state: RootState): string => state.user.idToken;
 
 export default userReducer.reducer;
---
+++ server/src/app-config/firebase-config.ts
@@ -0,0 +1,21 @@
+import * as admin from 'firebase-admin';
+import { Request, Response, NextFunction } from 'express';
+
+admin.initializeApp();
+
+type MiddlewareFn = (req: Request, res: Response, next: NextFunction) => void;
+
+//Verify token
+export const verifyToken: MiddlewareFn = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token || token === '') {
+        return res.status(404).send('No token provided');
+    }
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        res.locals.decodedToken = decodedToken;
+    } catch (e) {
+        return res.status(401).send(`invalid token: ${e}`);
+    }
+    next();
+};
