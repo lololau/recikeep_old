ebb48edd72fd503c29005fc8ddb498453ce61b52
lololau <verhille.lauriane@gmail.com>
2021-03-10 16:58:43 +0100

    add IngredientList component when we add a new ingredient to a recipe

--- client/src/containers/new-recipe/NewRecipe.tsx
+++ client/src/containers/new-recipe/NewRecipe.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { useTranslation } from 'react-i18next';
 import TextField from '@material-ui/core/TextField';
@@ -8,19 +8,78 @@ import { PartsComboBox } from '../stepper/RecipesSelection2';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
+import { ListItem, ListItemText, ListItemSecondaryAction } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import List from '@material-ui/core/List';
 import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
 import Autosuggestion from '../../components/Autocomplete';
 import { useSelector, useDispatch } from 'react-redux';
 import { ingredients, fetchAddIngredient } from '../../slice/ingredients/ingredientsSlice';
 import { unities, fetchAddUnity } from '../../slice/unity/unitySlice';
 
+type onRemove = (ingredient: IngredientRecipe, index: number) => void;
+
+type IngredientsListProps = {
+    ingredientsList: IngredientRecipe[];
+    onRemoveIngredient: onRemove;
+};
+
+interface IngredientRecipe {
+    name: string;
+    unity: string;
+    quantity: number;
+}
+
+const IngredientsList = (props: IngredientsListProps): JSX.Element => {
+    return (
+        <List>
+            {props.ingredientsList.map((ingredient, index) => {
+                return (
+                    <ListItem divider={true} key={index}>
+                        <ListItemText primary={ingredient.name} id={index.toString()} />
+                        <ListItemText primary={ingredient.quantity} id={index.toString()} />
+                        <ListItemText primary={ingredient.unity} id={index.toString()} />
+                        <ListItemSecondaryAction>
+                            <IconButton
+                                edge="end"
+                                onClick={() => {
+                                    if (props.onRemoveIngredient) {
+                                        props.onRemoveIngredient(ingredient, index);
+                                    }
+                                }}
+                            >
+                                <DeleteIcon style={{ fontSize: 15 }} color="primary" />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
+};
+
 const NewRecipe = (): JSX.Element => {
     const { t } = useTranslation();
 
     const dispatch = useDispatch();
 
-    const ingredientsList = useSelector(ingredients);
-    const unitiesList = useSelector(unities);
+    const allIngredients = useSelector(ingredients);
+    const allUnities = useSelector(unities);
+
+    const [ingredientRow, setIngredientRow] = useState<IngredientRecipe[]>([]);
+
+    const [nameIngredient, setNameIngredient] = useState('');
+    const [unityIngredient, setUnityIngredient] = useState('');
+    const [quantityIngredient, setQuantityIngredient] = useState(0);
+
+    const ingredient: IngredientRecipe = { name: nameIngredient, unity: unityIngredient, quantity: quantityIngredient };
+
+    const removeIngredientList = (elt: IngredientRecipe, index: number) => {
+        if (ingredientRow[index]) {
+            const newingredientRow = ingredientRow.filter((_, i) => i !== index);
+            setIngredientRow(newingredientRow);
+        }
+    };
 
     return (
         <Container>
@@ -84,26 +143,38 @@ const NewRecipe = (): JSX.Element => {
                     <Grid item xs={3}>
                         <Autosuggestion
                             label="add ingredient"
-                            onSelect={(option) => console.log('selected', option)}
+                            onSelect={(option) => setNameIngredient(option.name)}
                             onAdd={(option) => dispatch(fetchAddIngredient(option))}
-                            options={ingredientsList}
+                            options={allIngredients}
                         />
                     </Grid>
                     <Grid item xs={3}>
                         <Autosuggestion
                             label="add unit"
-                            onSelect={(option) => console.log('selected', option)}
+                            onSelect={(option) => setUnityIngredient(option.name)}
                             onAdd={(option) => dispatch(fetchAddUnity(option))}
-                            options={unitiesList}
+                            options={allUnities}
                         />
                     </Grid>
                     <Grid item xs={3}>
-                        <TextField placeholder="Quantity" variant="outlined" />
+                        <TextField
+                            placeholder="Quantity"
+                            variant="outlined"
+                            onChange={(event) => setQuantityIngredient(Number(event.currentTarget.value))}
+                        />
                     </Grid>
                     <Grid item xs={3}>
-                        <Button>{t('new_recipe.add')}</Button>
+                        <Button
+                            onClick={() => {
+                                const newIngredientRow = ingredientRow.concat(ingredient);
+                                setIngredientRow(newIngredientRow);
+                            }}
+                        >
+                            {t('new_recipe.add')}
+                        </Button>
                     </Grid>
                 </Grid>
+                <IngredientsList ingredientsList={ingredientRow} onRemoveIngredient={removeIngredientList} />
                 <Box style={{ width: '100%' }}>
                     <IconButton>
                         <LibraryAddIcon style={{ fontSize: 25, marginLeft: 'auto', marginRight: 'auto' }} />
