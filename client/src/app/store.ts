4ccee861c7ae5820e301e2ab49b24a4b10bfab63
lololau <verhille.lauriane@gmail.com>
2021-03-16 17:53:45 +0100

    resolve warning

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -21,6 +21,7 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
