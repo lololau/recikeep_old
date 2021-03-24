757f720558e98c6a27d961b6d858af66aac00869
lololau <verhille.lauriane@gmail.com>
2021-03-24 17:43:23 +0100

    numberPartsByRecipe send to the stepper

--- client/src/containers/stepper/RecipesSelection2.tsx
+++ client/src/containers/stepper/RecipesSelection2.tsx
@@ -1,21 +1,26 @@
 import { useTranslation } from 'react-i18next';
 import List from '@material-ui/core/List';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 // import { selectRecipes } from '../../slice/recipes/recipesSlice';
 // import { useSelector } from 'react-redux';
 import { Recipe } from '../../slice/recipes/recipesFetch';
-import { IngredientsRecipe } from '../../slice/ingredients/ingredientsFetch';
 import { Button } from '@material-ui/core';
 
-type onPartsSelected = (numberPartsByRecipe: IngredientsRecipe[]) => void;
+export type numberPartsRecipe = {
+    recipe_id: number;
+    number_parts: number;
+};
+
+type onPartsSelected = (numberPartsByRecipe: numberPartsRecipe[]) => void;
 
 interface SelectionPartsRecipeProps {
     recipes: Recipe[];
-    onPartsSelected?: onPartsSelected;
+    onPartsSelected: onPartsSelected;
 }
 
 const SelectionPartsRecipes: FC<SelectionPartsRecipeProps> = (props) => {
@@ -26,25 +31,41 @@ const SelectionPartsRecipes: FC<SelectionPartsRecipeProps> = (props) => {
     const initialState = props.recipes.map(() => defaultValue);
     const [parts, setParts] = useState<number[]>(initialState);
 
+    useEffect(() => {
+        let finalPartsRecipe: numberPartsRecipe[] = [];
+        finalPartsRecipe = props.recipes.map((recipe, index) => ({ number_parts: parts[index], recipe_id: recipe.id }));
+
+        props.onPartsSelected(finalPartsRecipe);
+    }, [parts]);
+
     return (
         <>
-            <TextField
-                style={{ width: '10%', textAlign: 'center' }}
-                placeholder={t('new_recipe.parts_add')}
-                value={defaultValue}
-                onChange={(event) => {
-                    const value = Number(event.currentTarget.value);
-                    setDefaultValue(value);
-                }}
-            />
-            <Button
-                onClick={() => {
-                    const newValues = props.recipes.map(() => defaultValue);
-                    setParts(newValues);
-                }}
-            >
-                {t('stepper.new-number-parts')}
-            </Button>
+            <Grid container spacing={0} style={{ alignItems: 'center', display: 'flex' }}>
+                <Grid item xs={2}>
+                    <TextField
+                        style={{ width: '100%', textAlign: 'center' }}
+                        placeholder={t('new_recipe.parts_add')}
+                        value={defaultValue}
+                        variant="outlined"
+                        onChange={(event) => {
+                            const value = Number(event.currentTarget.value);
+                            if (value > 0 || value < 1000) {
+                                setDefaultValue(value);
+                            }
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button
+                        onClick={() => {
+                            const newValues = props.recipes.map(() => defaultValue);
+                            setParts(newValues);
+                        }}
+                    >
+                        {t('stepper.new-number-parts')}
+                    </Button>
+                </Grid>
+            </Grid>
             <List>
                 {props.recipes.map((recipe, index) => {
                     const newState = [...parts];
@@ -58,8 +79,10 @@ const SelectionPartsRecipes: FC<SelectionPartsRecipeProps> = (props) => {
                                 placeholder={t('new_recipe.parts_add')}
                                 onChange={(event) => {
                                     const newParts = Number(event.currentTarget.value);
-                                    newState[index] = newParts;
-                                    setParts(newState);
+                                    if (newParts >= 0 && newParts < 1000) {
+                                        newState[index] = newParts;
+                                        setParts(newState);
+                                    }
                                 }}
                             />
                         </ListItem>
@@ -70,11 +93,9 @@ const SelectionPartsRecipes: FC<SelectionPartsRecipeProps> = (props) => {
     );
 };
 
-type onValidateNumberParts = (groceryList: IngredientsRecipe[]) => void;
-
 interface SelectionPartsProps {
     recipes: Recipe[];
-    onValidateNumberParts: onValidateNumberParts;
+    onValidateNumberParts: onPartsSelected;
 }
 
 const SelectionParts: FC<SelectionPartsProps> = (props): JSX.Element => {
@@ -85,7 +106,14 @@ const SelectionParts: FC<SelectionPartsProps> = (props): JSX.Element => {
             <h1>{t('stepper.title-part')}</h1>
 
             <div className="SelectionRecipesList">
-                <SelectionPartsRecipes recipes={props.recipes} />
+                <SelectionPartsRecipes
+                    recipes={props.recipes}
+                    onPartsSelected={(numberPartsByRecipe) => {
+                        if (props.onValidateNumberParts) {
+                            props.onValidateNumberParts(numberPartsByRecipe);
+                        }
+                    }}
+                />
             </div>
         </Container>
     );
