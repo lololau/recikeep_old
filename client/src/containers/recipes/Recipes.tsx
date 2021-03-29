3e14b1710afab2dfc9b1e79a550b025d9092d4d8
lololau <verhille.lauriane@gmail.com>
2021-03-29 17:29:39 +0200

    add modal to delete a recipe

--- client/src/containers/recipes/Recipes.tsx
+++ client/src/containers/recipes/Recipes.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Modal from '@material-ui/core/Modal';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import SearchBar from '../../components/SearchBar';
@@ -27,43 +28,98 @@ export type RecipesListProps = {
 
 export const RecipesList = (props: RecipesListProps): JSX.Element => {
     const dispatch = useDispatch();
-
+    const { t } = useTranslation();
     const history = useHistory();
 
+    const [modalOpen, setModalOpen] = useState(false);
+    const [toDeleteId, setToDeleteId] = useState(0);
+
+    const handleModalOpen = (id: number) => {
+        setToDeleteId(id);
+        setModalOpen(true);
+    };
+
+    const handleClose = () => {
+        setModalOpen(false);
+    };
+
+    const bodyModal = (
+        <Grid
+            container
+            spacing={4}
+            style={{
+                marginTop: '25%',
+                maxWidth: 350,
+                marginRight: 'auto',
+                marginLeft: 'auto',
+                flexDirection: 'column',
+                backgroundColor: 'white',
+                alignItems: 'center',
+            }}
+        >
+            <Grid item xs>
+                <h2>{t('modal.delete-recipe')}</h2>
+                <p>{t('modal.delete-definitif')}</p>
+            </Grid>
+            <Grid item xs>
+                <Grid container spacing={4} style={{}}>
+                    <Grid item xs={6}>
+                        <Button
+                            onClick={() => {
+                                dispatch(fetchDeleteRecipe(toDeleteId));
+                                handleClose();
+                            }}
+                        >
+                            {t('modal.delete-yes')}
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button onClick={handleClose}>{t('modal.delete-no')}</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+
     return (
-        <List>
-            {props.recipes.map((recipe, index) => {
-                return (
-                    <ListItem divider={true} key={'RecipesList' + index}>
-                        <Link to={'/recipe/' + recipe.id}>
-                            <ListItemText
-                                onClick={() => dispatch(fetchGetARecipe(recipe.id))}
-                                primary={recipe.name}
-                                id={index.toString()}
-                            />
-                        </Link>
-                        <ListItemSecondaryAction>
-                            <IconButton
-                                onClick={() => {
-                                    history.push(`/recipes/update/${recipe.id}`);
-                                    console.log(recipe);
-                                }}
-                            >
-                                <EditIcon style={{ fontSize: 15 }} color="primary" />
-                            </IconButton>
-                            <IconButton
-                                edge="end"
-                                onClick={() => {
-                                    dispatch(fetchDeleteRecipe(recipe.id));
-                                }}
-                            >
-                                <DeleteIcon style={{ fontSize: 15 }} color="primary" />
-                            </IconButton>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                );
-            })}
-        </List>
+        <>
+            <List>
+                {props.recipes.map((recipe, index) => {
+                    return (
+                        <ListItem divider={true} key={'RecipesList' + index}>
+                            <Link to={'/recipe/' + recipe.id}>
+                                <ListItemText
+                                    onClick={() => dispatch(fetchGetARecipe(recipe.id))}
+                                    primary={recipe.name}
+                                    id={index.toString()}
+                                />
+                            </Link>
+                            <ListItemSecondaryAction>
+                                <IconButton
+                                    onClick={() => {
+                                        history.push(`/recipes/update/${recipe.id}`);
+                                        console.log(recipe);
+                                    }}
+                                >
+                                    <EditIcon style={{ fontSize: 15 }} color="primary" />
+                                </IconButton>
+                                <IconButton edge="end" onClick={() => handleModalOpen(recipe.id)}>
+                                    <DeleteIcon style={{ fontSize: 15 }} color="primary" />
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    );
+                })}
+            </List>
+            <Modal
+                open={modalOpen}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {bodyModal}
+            </Modal>
+        </>
     );
 };
 
