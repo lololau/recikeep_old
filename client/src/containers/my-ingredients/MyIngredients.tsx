5fbd023f3fbac247ae19a7af10a1a28ab348a766
lololau <verhille.lauriane@gmail.com>
2021-03-29 17:39:37 +0200

    remove the adding button on MyIngredients container

--- client/src/containers/my-ingredients/MyIngredients.tsx
+++ client/src/containers/my-ingredients/MyIngredients.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { useTranslation } from 'react-i18next';
-import { Button, IconButton, TextField } from '@material-ui/core';
-import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import Grid from '@material-ui/core/Grid';
-import { Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
 import SearchBar from '../../components/SearchBar';
 import ListComponent, { Element } from '../../components/List';
 import { Ingredient } from '../../slice/ingredients/ingredientsFetch';
@@ -16,8 +13,6 @@ const MyIngredients = (): JSX.Element => {
     const { t } = useTranslation();
 
     const dispatch = useDispatch();
-
-    const [modalOpen, setModalOpen] = useState(false);
 
     const selectIngredientsCustom = (ingredientsElements: Ingredient[]): Ingredient[] =>
         ingredientsElements.filter((ingredient) => {
@@ -54,45 +49,12 @@ const MyIngredients = (): JSX.Element => {
                 <Grid item xs={6}>
                     <h1>{t('myIngredients.title-page')}</h1>
                 </Grid>
-                <Grid item xs={6} style={{ textAlign: 'right', alignItems: 'center' }}>
-                    <IconButton onClick={() => setModalOpen(true)}>
-                        <AddCircleOutlineOutlinedIcon style={{ fontSize: 30 }} />
-                    </IconButton>
-                </Grid>
             </Grid>
             <SearchBar elements={ingredientsList} onchange={onChange} width={'50%'} />
             <ListComponent
                 listElements={selectIngredientsCustom(ingredientsDisplay)}
                 onRemoveElement={deleteIngredient}
             />
-            <Dialog open={modalOpen} style={{}}>
-                <Container>
-                    <DialogTitle>
-                        <h3>{t('myIngredients.add-ingredient')}</h3>
-                    </DialogTitle>
-                    <DialogContent>
-                        <DialogActions>
-                            <Grid container spacing={4} style={{ flexDirection: 'column' }}>
-                                <Grid item xs>
-                                    <Grid container spacing={6} style={{ alignItems: 'center' }}>
-                                        <Grid item xs={6}>
-                                            <TextField placeholder={t('myIngredients.textField')} />
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Button>{t('myIngredients.modal-add')}</Button>
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs>
-                                    <Button onClick={() => setModalOpen(false)}>
-                                        {t('myIngredients.modal-close')}
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </DialogActions>
-                    </DialogContent>
-                </Container>
-            </Dialog>
         </Container>
     );
 };
