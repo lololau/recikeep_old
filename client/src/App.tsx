c758e94b11aebdec3f8475d374c36dc859cc3718
lololau <verhille.lauriane@gmail.com>
2021-04-01 11:35:03 +0200

    update recipe container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -88,14 +88,15 @@ const App = (): JSX.Element => {
                     <div className="App">
                         <Route path="/recipes" exact component={HomeRecipes} />
                         <Route path="/profile" exact component={Profile} />
-                        <Route path="/recipe/:id">
-                            <MyRecipe />
-                        </Route>
                         <Route path={'/recipes/selection'} exact component={RecipesSelectionStepper} />
                         <Route path={'/profile/my_ingredients'} component={MyIngredients} />
                         <Route path={'/profile/my_unities'} component={MyUnities} />
                         <Route path="/recipes/update/:id" exact>
                             <UpdateRecipe />
+                        </Route>
+
+                        <Route path="/recipe/:id">
+                            <MyRecipe />
                         </Route>
                         <Route path="/groceryList/:id">
                             <GroceryList />
