c5744a3ee3cd8e16d4da9a2923bc61d8eb0a0248
lololau <verhille.lauriane@gmail.com>
2021-04-22 13:20:42 +0200

    update translation files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
 import './app/config';
